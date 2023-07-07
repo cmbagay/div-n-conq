27ced2873e2c393a0abe6d9d594125b6dad28a2b
cmbagay <cbabagaygm@gmail.com>
2023-07-07 11:45:58 -0700

    reset code

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,17 +1,5 @@
-function countZeroes(arr) {
-  let zeroArr = arr;
-
-  while (zeroArr[0] != 0) {
-    const arrLength = zeroArr.length - 1;
-    const median = Math.floor(arrLength / 2);
-
-    if (zeroArr[median] == 0) {
-      zeroArr = zeroArr.slice(median, arrLength);
-      return arrLength;
-    } else {
-      zeroArr = zeroArr.slice(median + 1, arrLength);
-    }
-  }
+function countZeroes() {
+  
 }
 
-module.exports = countZeroes;
+module.exports = countZeroes
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,14 +1,5 @@
-function sortedFrequency(arr, num) {
-  let numArr = arr;
+function sortedFrequency() {
 
-  while (numArr[0] != num) {
-    const arrLength = numArr.length - 1;
-    const median = Math.floor(arrLength / 2);
-
-    if (numArr[median] == num) {
-    } else {
-    }
-  }
 }
 
-module.exports = sortedFrequency;
+module.exports = sortedFrequency
